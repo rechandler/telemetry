c8a9f828bfb21e2a7c6591b31ea0e7e42eaa52b3
rechandler <ryann.chandler@gmail.com>
2022-02-21 14:12:39 -0600

    adds position and launching options to ui

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 const { app, BrowserWindow, autoUpdater, dialog, ipcMain } = require('electron')
+const Store = require('electron-store')
+
 
 if (require('electron-squirrel-startup')) return;
 
-const iracing = require('node-irsdk').getInstance()
-const state = {
-  telemetry: false
+const schema = {
+  "telemetryWidget": {
+    "type": "object",
+    "description": "telemetry widgets options",
+    "properties": {
+      "displayed": {
+        "type": "boolean",
+        "description": "whether or not the widget is on the screen"
+      },
+      "position": {
+        "type": "object",
+        "properties": {
+          "x": { "type": "number"},
+          "y": { "type": "number"}
+        }
+      },
+      "positionEnabled": {
+        "type": "boolean",
+        "description": "whether or not to achnowledge the x, y cooredinates of the last move"
+      },
+      "launchEnabled": {
+        "type": "boolean",
+        "description": "whether or not to launch the widget on startup"
+      }
+    }
+  }
 }
+
+const store = new Store({schema})
+
+const iracing = require('node-irsdk').getInstance()
 
 // const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000
 const server = 'https://telemetry-lc9vbg16r-rechandler.vercel.app'
@@ -34,8 +63,13 @@ function createWindow () {
   }
 
   win.on('close', () => {
+    store.set('telemetryWidget.displayed', false)
     app.quit()
   })
+
+  if(store.get('telemetryWidget.launchEnabled')) {
+    launchTelemetry()
+  }
 }
 
 // This method will be called when Electron has finished
@@ -86,11 +120,20 @@ const setupAutoUpdater = () => {
 // code. You can also put them in separate files and require them here.
 
 ipcMain.on('telemetryLaunch', () => {
+  launchTelemetry()
+})
 
-  if (state.telemetry) return
+const updateWindowPosition = ({x, y}, windowName) => {
+  store.set(`${windowName}.position.x`, x)
+  store.set(`${windowName}.position.y`, y)
+}
 
-  // Create the browser window.
-  const telemetryWindow = new BrowserWindow({
+const launchTelemetry = () => {
+  if (store.get('telemetryWidget.displayed')) return
+
+  const telemStore = store.get('telemetryWidget')
+
+  let browserOptions = {
     width: 565,
     height: 180,
     frame: false,
@@ -100,7 +143,19 @@ ipcMain.on('telemetryLaunch', () => {
       contextIsolation: false,
       additionalArguments: ["Telemetry"]
     }
-  })
+  }
+
+  if (telemStore && telemStore.position && telemStore.positionEnabled) {
+    const {x, y} = telemStore.position
+    browserOptions = {
+      ...browserOptions,
+      x,
+      y
+    }
+  }
+
+  // Create the browser window.
+  const telemetryWindow = new BrowserWindow(browserOptions)
 
   if(app.isPackaged) {
     // Production Mode
@@ -123,9 +178,13 @@ ipcMain.on('telemetryLaunch', () => {
 
   // Cancel the irsdk telemetry events
   telemetryWindow.on('closed', () => {
+    store.set('telemetryWidget.displayed', false)
     _stop()
-    state.telemetry = false
   })
 
-  state.telemetry = true
-})
+  telemetryWindow.on('move', () => {
+    updateWindowPosition(telemetryWindow.getBounds(), 'telemetryWidget')
+  })
+
+  store.set('telemetryWidget.displayed', true)
+}
--- src/components/telemetry/telemetryMenu.js
+++ src/components/telemetry/telemetryMenu.js
@@ -1,10 +1,29 @@
-import Telemetry from './telemetry'
-
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
+const Store = window.require('electron-store');
 const { ipcRenderer } = window.require('electron')
+
+const store = new Store()
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default () => {
     
     const launchTelemetryWidget = () => ipcRenderer.send('telemetryLaunch')
+    const [launchEnabled, setLaunchEnabled] = useState(store.get('telemetryWidget.launchEnabled') || false)
+    const [positionEnabled, setPositionEnabled] = useState(store.get('telemetryWidget.positionEnabled') || false)
+
+    const updateLaunchEnabled = value => {
+        store.set('telemetryWidget.launchEnabled', value)
+        setLaunchEnabled(value)
+    }
+
+    const updatePositionEnabled = value => {
+        store.set('telemetryWidget.positionEnabled', value)
+        setPositionEnabled(value)
+    }
 
     return (
         <div className="py-6">
@@ -14,9 +33,51 @@ export default () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
             {/* Replace with your content */}
             <div className="py-4">
+                <Switch.Group as="div" className="flex items-center p-2">
+                    <Switch
+                        checked={launchEnabled}
+                        onChange={updateLaunchEnabled}
+                        className={classNames(
+                        launchEnabled ? 'bg-sky-600' : 'bg-gray-200',
+                        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500'
+                        )}
+                    >
+                        <span
+                        aria-hidden="true"
+                        className={classNames(
+                            launchEnabled ? 'translate-x-5' : 'translate-x-0',
+                            'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+                        )}
+                        />
+                    </Switch>
+                    <Switch.Label as="span" className="ml-3">
+                        <span className="text-sm font-medium text-gray-900">Launch on Startup</span>
+                    </Switch.Label>
+                </Switch.Group>
+                <Switch.Group as="div" className="flex items-center p-2">
+                    <Switch
+                        checked={positionEnabled}
+                        onChange={updatePositionEnabled}
+                        className={classNames(
+                        positionEnabled ? 'bg-sky-600' : 'bg-gray-200',
+                        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500'
+                        )}
+                    >
+                        <span
+                        aria-hidden="true"
+                        className={classNames(
+                            positionEnabled ? 'translate-x-5' : 'translate-x-0',
+                            'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+                        )}
+                        />
+                    </Switch>
+                    <Switch.Label as="span" className="ml-3">
+                        <span className="text-sm font-medium text-gray-900">Save widget position on move</span>
+                    </Switch.Label>
+                </Switch.Group>
                 <button
                     type="button"
-                    className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                    className="inline-flex items-center px-2.5 py-1.5 mt-5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
                     onClick={launchTelemetryWidget}
                 >
                     Launch Telemetry Widget
